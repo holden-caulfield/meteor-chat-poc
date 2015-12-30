e1c709a20809311e2956f491cd207f4f7ec332f3
JP Saraceno <jsarac@gmail.com>
2015-12-30 11:58:21 -0300

    fist shot, single chatroom, works on web, android and iOS

---
+++ mobile-config.js
@@ -0,0 +1,9 @@
+App.info({
+  name: 'chatpoc',
+  description: 'A simple proof of concept chat built in Meteor.',
+  author: 'JP Saraceno',
+  email: 'jp@scvsoft.com',
+  website: 'http://www.scvsoft.com'
+});
+
+App.setPreference('StatusBarOverlaysWebView', 'false');
