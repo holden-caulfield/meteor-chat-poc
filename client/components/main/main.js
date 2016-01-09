f8dd8ed2c8c71b8980597c7700d24a27908802ad
JP Saraceno <jsarac@gmail.com>
2016-01-08 22:31:04 -0300

    Promote user and room to global helpers

--- client/components/main/main.js
+++ client/components/main/main.js
@@ -1,12 +1,3 @@
-Template.main.helpers({
-    user: function() {
-      return Session.get("user");
-    },
-    room: function() {
-      return Session.get("room"); 
-    }
-})
-
 Presence.state = function() {
   return {
     username: Session.get('user')
--- client/lib/helpers.js
+++ client/lib/helpers.js
@@ -2,3 +2,12 @@ scrollChat = function() {
     var elem = document.getElementById('chat-history');
     elem.scrollTop = elem.scrollHeight;
 }
+
+
+Template.registerHelper("user", function() {
+  return Session.get("user");
+});
+
+Template.registerHelper("room", function() {
+  return Session.get("room");
+});
